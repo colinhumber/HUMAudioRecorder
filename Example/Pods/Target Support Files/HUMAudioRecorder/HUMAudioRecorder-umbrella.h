0304b8d8cd08faebdcfe6e8a5e6c1a025345dbd8
Colin Humber <colinhumber@gmail.com>
2015-11-02 15:35:47 -0700

    Added missing pod files

---
+++ Example/Pods/Target Support Files/HUMAudioRecorder/HUMAudioRecorder-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "HUMAudioRecorder.h"
+
+FOUNDATION_EXPORT double HUMAudioRecorderVersionNumber;
+FOUNDATION_EXPORT const unsigned char HUMAudioRecorderVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-HUMAudioRecorder_Example/Pods-HUMAudioRecorder_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HUMAudioRecorder_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HUMAudioRecorder_ExampleVersionString[];
+
